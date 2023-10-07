c93ad887397d4e8b9bdb6bdf50587e8fdbe5d45a
imdhson <hhammer1234@gmail.com>
2023-10-07 20:00:53 +0900

    ScrapSender Prototyping

--- modules/AI_ScrapSender.go
+++ modules/AI_ScrapSender.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"os"
+	"sort"
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -46,4 +47,42 @@ func ScrapSender(w http.ResponseWriter, r *http.Request) { //메인화면 시설
 	for _, v := range will_send_ARR {
 		will_send_ARR.serviceScoreAdd(user_struct.Settings, v)
 	}
+	//score을 기반으로 sort 시작
+	sort.Sort(sort.Reverse(will_send_ARR))
+	ai_list_num := 0
+	for numi, _ := range will_send_ARR {
+		will_send_ARR[numi].AI_List_num = ai_list_num
+		ai_list_num++
+	}
+
+	var Outputsize_var int               //결과 슬라이싱시 인덱스 바깥으로 튀는것 방지하기 위함
+	if len(will_send_ARR) < OUTPUTSIZE { //결과 슬라이싱시 인덱스 바깥으로 튀는것 방지하기 위함
+		Outputsize_var = len(will_send_ARR)
+	} else {
+		Outputsize_var = OUTPUTSIZE
+	}
+
+	//필요한 만큼 outputsize로 자르고 메인에서 필요한 데이터만 남김
+	var will_send_refined []SO_jobs_refined
+	for ir, vr := range will_send_ARR {
+		if ir > Outputsize_var {
+			break
+		}
+		tmp_address1 := vr.Loc_1 + " " + vr.Loc_2
+		tmp := SO_jobs_refined{
+			AI_List_num:    vr.AI_List_num,
+			ID:             vr.ID,
+			NameofFacility: vr.NameofFacility,
+			TypeofFacility: vr.TypeofFacility,
+			Address:        tmp_address1,
+			Operator:       vr.Operator,
+			ViewCount:      vr.ViewCount,
+		}
+		will_send_refined = append(will_send_refined, tmp)
+	}
+
+	will_send_json, _ := json.MarshalIndent(will_send_refined, " ", "	")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(will_send_json)
 }
